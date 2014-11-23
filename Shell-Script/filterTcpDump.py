403e70a5709e5e25e0afae33ae27e0b8c9aa781d
Pushkar D. Joglekar <pushkar.joglekar1789@gmail.com>
2014-11-23 13:12:13 -0800

    Bug fixes for the script that parse through the text file to send AppName and timestamp of the packet

--- Shell-Script/filterTcpDump.py
+++ Shell-Script/filterTcpDump.py
@@ -1,12 +1,14 @@
 import subprocess
 import urllib2
 import time
+import StringIO
+
 class ProcessDump:
 	def getAppName(self, Url):
 		if "secure.splitwise.com" in Url:
 			return "Splitwise"
-		if "www.chase.com" in output:
-			return "Chase Bank";
+		if "wwwbcchase.gslb.bankone.com" in Url:
+			return "ChaseBank"
 
 			#epoch_time= int(time.time())
 		#urllib2.urlopen('http://1-dot-ninjabotscmu.appspot.com/ninjabotscmu?data='+appName+"+"+str(epoch_time))
@@ -26,12 +28,13 @@ output = getFirstWinDump.communicate()[0]
 processId= output.split()[1]
 subprocess.Popen("taskkill /f /PID " + processId)
 
-file = subprocess.Popen("tshark -nr DumpFile01.pcap -T text -V")
-file.communicate()[0]
+file = subprocess.Popen("tshark -nr DumpFile01.pcap -T text -V",stdout=subprocess.PIPE)
+outputtext = file.communicate()[0]
 
 #Ref: http://cmdlinetips.com/2011/08/three-ways-to-read-a-text-file-line-by-line-in-python/
 ## Open the file with read only permit
-f = file
+f = StringIO.StringIO(outputtext)
+#print f
 ## Read the first line 
 line = f.readline()
 ## If the file is not empty keep reading line one at a time
@@ -41,32 +44,38 @@ oldAppName =""
 appName=""
 epoch_time=""
 while line:
-    print line
-    line = f.readline()
-    if "Epoch Time:" in line:
-    	timestamp = line.split(":")
-    	epoch_time = timestamp[0].split()
-    if "Answers" in line:
-    	line = f.readline()
-   		dnsAnswer[] = line.split()
-   		Url = dnsAnswer[0][:-1]
-   		IP = dnsAnswer[2][4:]
-   		appName = getAppName(Url)
-   		IPAppDict[IP]=appName
-   	if "Internet Protocol Version 4" in line:
-   		line = f.readline()
-   		IPHeader = line.split()
-   		serverIP = IPHeader[1][4:]
-   		if serverIP in dict.keys():
-   			appName = IPAppDict[serverIP]
-   			if oldAppName != appName
-				urllib2.urlopen('http://1-dot-ninjabotscmu.appspot.com/ninjabotscmu?data='+oldAppName+"+"+str(epoch_time))
-				oldAppName = appName
+		line = f.readline()
+		if "Epoch Time:" in line:
+			timestamp = line.split(":")
+			epoch_time = timestamp[1].split()[0]
+		if "Answers" in line:
+			line = f.readline()
+			while "type A" not in line and line != "":
+				line = f.readline()
+			dnsAnswer = line.split()
+			Url = dnsAnswer[0][:-1]
+			IP = dnsAnswer[6]
+			p = ProcessDump()
+			knownAppName = p.getAppName(Url)
+			if knownAppName != None:
+				appName = knownAppName
+			IPAppDict[IP]=appName
+			if "Internet Protocol Version 4" in line:
+				line = f.readline()
+				IPHeader = line.split()
+				serverIP = IPHeader[1][4:]
+				if serverIP in dict.keys():
+					appName = IPAppDict[serverIP]
+				if oldAppName != appName:
+					urllib2.urlopen('http://1-dot-ninjabotscmu.appspot.com/ninjabotscmu?data='+oldAppName+"+"+str(epoch_time))
+					oldAppName = appName
+print appName
+print epoch_time
+print int(time.time())
+urllib2.urlopen('http://1-dot-ninjabotscmu.appspot.com/ninjabotscmu?data='+appName+"+"+epoch_time)
+f.close() 
 
-urllib2.urlopen('http://1-dot-ninjabotscmu.appspot.com/ninjabotscmu?data='+appName+"+"+str(epoch_time))
-f.close()	
-
-if output != None and output != "":
-	p = ProcessDump()
-	p.sendData(output)		
-print output
+# if output != None and output != "":
+# 	p = ProcessDump()
+# 	p.sendData(output)    
+# #print output
